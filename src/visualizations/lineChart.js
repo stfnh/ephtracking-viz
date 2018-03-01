59d94a0a83a3e6b3cb0b244153e6a3f22cbfec83
Stefan Hochwarter <stefan.hochwarter@gmail.com>
2018-03-01 13:48:10 -0500

    feat(line-chart): show no data

--- src/visualizations/lineChart.js
+++ src/visualizations/lineChart.js
@@ -16,50 +16,54 @@ const lineChart = (container, data) => {
         dataValue: parseInt(item.dataValue, 10),
         displayValue: item.displayValue
       }));
+      // data is in form: year, dataValue, displayValue (all string)
+      // const height = .log(d3.select('.bar').node().style.width);
+      // --- create d3 line chart ---
+
+      const svg = d3.select(container);
+      const margin = { top: 20, right: 20, bottom: 30, left: 50 };
+      const width = +svg.attr('width') - margin.left - margin.right;
+      const height = +svg.attr('height') - margin.top - margin.bottom;
+      const g = svg
+        .append('g')
+        .attr('transform', `translate(${margin.left},${margin.top})`);
+
+      const x = d3.scaleBand().range([0, width]);
+      const y = d3.scaleLinear().rangeRound([height, 0]);
+
+      x.domain(preparedData.map(d => d.year));
+      y.domain(d3.extent(preparedData, d => d.dataValue));
+
+      const line = d3
+        .line()
+        .x(d => x(d.year))
+        .y(d => y(d.dataValue));
+
+      g
+        .append('g')
+        .attr('transform', `translate(0, ${height})`)
+        .call(d3.axisBottom(x))
+        .select('.domain');
+
+      g.append('g').call(d3.axisLeft(y));
+
+      g
+        .append('path')
+        .datum(preparedData)
+        .attr('fill', 'none')
+        .attr('stroke', 'steelblue')
+        .attr('stroke-linejoin', 'round')
+        .attr('stroke-linecap', 'round')
+        .attr('stroke-width', 1.5)
+        .attr('d', line);
     } else {
-      preparedData = null;
+      d3
+        .select(container)
+        .append('text')
+        .attr('y', '20')
+        .attr('font-family', 'monospace')
+        .text('no data');
     }
-
-    // data is in form: year, dataValue, displayValue (all string)
-    // const height = .log(d3.select('.bar').node().style.width);
-    // --- create d3 line chart ---
-
-    const svg = d3.select(container);
-    const margin = { top: 20, right: 20, bottom: 30, left: 50 };
-    const width = +svg.attr('width') - margin.left - margin.right;
-    const height = +svg.attr('height') - margin.top - margin.bottom;
-    const g = svg
-      .append('g')
-      .attr('transform', `translate(${margin.left},${margin.top})`);
-
-    const x = d3.scaleBand().range([0, width]);
-    const y = d3.scaleLinear().rangeRound([height, 0]);
-
-    x.domain(preparedData.map(d => d.year));
-    y.domain(d3.extent(preparedData, d => d.dataValue));
-
-    const line = d3
-      .line()
-      .x(d => x(d.year))
-      .y(d => y(d.dataValue));
-
-    g
-      .append('g')
-      .attr('transform', `translate(0, ${height})`)
-      .call(d3.axisBottom(x))
-      .select('.domain');
-
-    g.append('g').call(d3.axisLeft(y));
-
-    g
-      .append('path')
-      .datum(preparedData)
-      .attr('fill', 'none')
-      .attr('stroke', 'steelblue')
-      .attr('stroke-linejoin', 'round')
-      .attr('stroke-linecap', 'round')
-      .attr('stroke-width', 1.5)
-      .attr('d', line);
   });
 };
 
