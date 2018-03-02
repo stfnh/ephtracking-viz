4d3d494b1c56ae324b10ad960d461a56ccbbf7f2
Stefan Hochwarter <stefan.hochwarter@gmail.com>
2018-03-02 17:31:02 -0500

    feat(line chart): support of many states
    line chart can now draw many states (array)
    
    BREAKING CHANGE: data options for line charts changes (state -> states; array)

--- src/visualizations/lineChart.js
+++ src/visualizations/lineChart.js
@@ -1,25 +1,36 @@
 import * as d3 from 'd3';
+import ColorScheme from 'color-scheme';
 
 const lineChart = (container, data) => {
   const url = `https://ephtracking.cdc.gov/apigateway/api/v1/getData/${
     data.measureId
-  }/${data.state}/0/ALL/0/json`;
+  }/${data.states}/0/ALL/0/json`;
   d3.json(url, (error, response) => {
     if (error) {
       console.error(error);
-    }
-    // ToDo: Refactor
-    let preparedData;
-    if (response.tableResult && response.tableResult.length > 0) {
-      preparedData = response.tableResult.map(item => ({
+    } else if (response.tableResult && response.tableResult.length > 0) {
+      const preparedData = response.tableResult.map(item => ({
         year: item.year,
-        dataValue: parseInt(item.dataValue, 10),
-        displayValue: item.displayValue
+        dataValue: parseFloat(item.dataValue),
+        displayValue: item.displayValue,
+        geo: item.geo,
+        geoId: item.geoId,
+        geoAbbreviation: item.geoAbbreviation,
+        rollover: item.rollover[0]
       }));
       // data is in form: year, dataValue, displayValue (all string)
       // const height = .log(d3.select('.bar').node().style.width);
       // --- create d3 line chart ---
 
+      // color scheme
+      const scheme = new ColorScheme();
+      scheme
+        .from_hex('4863A0')
+        .scheme('tetrade')
+        .variation('pastel');
+      const colors = scheme.colors(); // array of 16 colors
+
+      // create line chart
       const svg = d3.select(container);
       const margin = { top: 20, right: 20, bottom: 30, left: 50 };
       const width = +svg.attr('width') - margin.left - margin.right;
@@ -34,28 +45,40 @@ const lineChart = (container, data) => {
       x.domain(preparedData.map(d => d.year));
       y.domain(d3.extent(preparedData, d => d.dataValue));
 
-      const line = d3
-        .line()
-        .x(d => x(d.year))
-        .y(d => y(d.dataValue));
-
       g
         .append('g')
         .attr('transform', `translate(0, ${height})`)
         .call(d3.axisBottom(x))
         .select('.domain');
-
       g.append('g').call(d3.axisLeft(y));
 
-      g
-        .append('path')
-        .datum(preparedData)
-        .attr('fill', 'none')
-        .attr('stroke', 'steelblue')
-        .attr('stroke-linejoin', 'round')
-        .attr('stroke-linecap', 'round')
-        .attr('stroke-width', 1.5)
-        .attr('d', line);
+      // group data by state
+      const lines = {};
+      preparedData.forEach(item => {
+        if (lines[item.geoId]) {
+          lines[item.geoId].push(item);
+        } else {
+          lines[item.geoId] = [item];
+        }
+      });
+
+      // draw lines
+      const line = d3
+        .line()
+        .x(d => x(d.year))
+        .y(d => y(d.dataValue));
+      const keys = Object.keys(lines);
+      keys.forEach((key, index) => {
+        g
+          .append('path')
+          .datum(lines[key])
+          .attr('fill', 'none')
+          .attr('stroke', `#${colors[index]}`)
+          .attr('stroke-linejoin', 'round')
+          .attr('stroke-linecap', 'round')
+          .attr('stroke-width', 1.5)
+          .attr('d', line);
+      });
     } else {
       d3
         .select(container)
